9f59a184e026014e42757b617d7ed62791e892b3
Juan SP <juan-220597@hotmail.com>
2024-10-11 17:55:47 -0600

    the sql files were modified to add more information

--- BD/database_oneclickcar_nofk.sql
+++ BD/database_oneclickcar_nofk.sql
@@ -15,14 +15,14 @@ CREATE SCHEMA IF NOT EXISTS `OneClickCar` DEFAULT CHARACTER SET utf8 ;
 USE `OneClickCar` ;
 
 -- -----------------------------------------------------
--- Table `OneClickCar`.`Users`
+-- Table `OneClickCar`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OneClickCar`.`Users` (
+CREATE TABLE IF NOT EXISTS `OneClickCar`.`users` (
   `id_user` INT UNSIGNED NOT NULL,
   `full_name` VARCHAR(80) NOT NULL,
   `phone_number` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `birth_date` DATE NOT NULL,
+  `birth_date` VARCHAR(10) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `type_user` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_user`))
@@ -31,14 +31,14 @@ COMMENT = '	';
 
 
 -- -----------------------------------------------------
--- Table `OneClickCar`.`Cars`
+-- Table `OneClickCar`.`cars`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OneClickCar`.`Cars` (
+CREATE TABLE IF NOT EXISTS `OneClickCar`.`cars` (
   `id_cars` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `type` VARCHAR(45) NOT NULL,
   `brand` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `year` YEAR NOT NULL,
+  `year` VARCHAR(10) NOT NULL,
   `kilometer` INT NOT NULL,
   `transmission` VARCHAR(45) NOT NULL,
   `price` INT UNSIGNED NOT NULL,
@@ -47,66 +47,68 @@ CREATE TABLE IF NOT EXISTS `OneClickCar`.`Cars` (
   `description` LONGTEXT NOT NULL,
   `verified` TINYINT UNSIGNED NOT NULL,
   `sold` TINYINT UNSIGNED NOT NULL,
-  `Users_id_user` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_cars`, `Users_id_user`))
+  `nu_serial` VARCHAR(17) NOT NULL,
+  `users_id_user` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_cars`, `users_id_user`),
+  UNIQUE INDEX `nu_serial_UNIQUE` (`nu_serial` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `OneClickCar`.`Comments`
+-- Table `OneClickCar`.`comments`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OneClickCar`.`Comments` (
+CREATE TABLE IF NOT EXISTS `OneClickCar`.`comments` (
   `id_comment` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `content` TEXT NOT NULL,
   `rating` INT NOT NULL,
-  `comment_date` DATE NOT NULL,
+  `comment_date` VARCHAR(10) NOT NULL,
   `approved` TINYINT NOT NULL,
-  `Sellers_id_seller` INT UNSIGNED NOT NULL,
-  `Users_id_user` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_comment`, `Sellers_id_seller`, `Users_id_user`))
+  `sellers_id_seller` INT UNSIGNED NOT NULL,
+  `users_id_user` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_comment`, `sellers_id_seller`, `users_id_user`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `OneClickCar`.`Payment_method`
+-- Table `OneClickCar`.`payment_methods`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OneClickCar`.`Payment_method` (
+CREATE TABLE IF NOT EXISTS `OneClickCar`.`payment_methods` (
   `id_card` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name_card` VARCHAR(45) NOT NULL,
   `type_card` VARCHAR(45) NOT NULL,
   `number_card` VARCHAR(16) NOT NULL,
-  `date_card` DATE NOT NULL,
+  `date_card` VARCHAR(10) NOT NULL,
   `cvv_card` VARCHAR(3) NOT NULL,
-  `Users_id_user` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_card`, `Users_id_user`))
+  `users_id_user` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_card`, `users_id_user`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `OneClickCar`.`Deposit_method`
+-- Table `OneClickCar`.`deposit_methods`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OneClickCar`.`Deposit_method` (
+CREATE TABLE IF NOT EXISTS `OneClickCar`.`deposit_methods` (
   `id_account` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name_account` VARCHAR(45) NOT NULL,
   `name_bank` VARCHAR(45) NOT NULL,
   `account_bank` VARCHAR(16) NOT NULL,
-  `Users_id_user` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_account`, `Users_id_user`))
+  `users_id_user` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_account`, `users_id_user`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `OneClickCar`.`Transactions`
+-- Table `OneClickCar`.`transactions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OneClickCar`.`Transactions` (
+CREATE TABLE IF NOT EXISTS `OneClickCar`.`transactions` (
   `id_transaction` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `date_transaction` DATE NOT NULL,
-  `Users_id_buyer` INT UNSIGNED NOT NULL,
-  `Users_id_seller` INT UNSIGNED NOT NULL,
-  `Cars_id_cars` INT UNSIGNED NOT NULL,
-  `Payment_method_id_card` INT UNSIGNED NOT NULL,
-  `Deposit_method_id_account` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_transaction`, `Users_id_buyer`, `Users_id_seller`, `Cars_id_cars`, `Payment_method_id_card`, `Deposit_method_id_account`))
+  `date_transaction` VARCHAR(10) NOT NULL,
+  `users_id_buyer` INT UNSIGNED NOT NULL,
+  `users_id_seller` INT UNSIGNED NOT NULL,
+  `cars_id_cars` INT UNSIGNED NOT NULL,
+  `payment_method_id_card` INT UNSIGNED NOT NULL,
+  `deposit_method_id_account` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_transaction`, `users_id_buyer`, `users_id_seller`, `cars_id_cars`, `payment_method_id_card`, `deposit_method_id_account`))
 ENGINE = InnoDB;
 
 
