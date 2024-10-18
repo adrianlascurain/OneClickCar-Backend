711a1af12cf003107930a2e8ee49ee26af268a5d
Adria Hernandez <ahernandezlasc@gmail.com>
2024-10-18 00:58:10 -0600

    test are deleted

--- src/test/java/com/ecommerce/oneclickcar/OneclickcarApplicationTests.java
+++ src/test/java/com/ecommerce/oneclickcar/OneclickcarApplicationTests.java
@@ -1,12 +1,5 @@
 package com.ecommerce.oneclickcar;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-@SpringBootTest
 class OneclickcarApplicationTests {
-	@Test
-	void contextLoads() {
-	}
+
 }
